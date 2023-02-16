92733b8459a7c1cfde1840bd3771e5fb3c038e09
Timur <timurtokaev.toombez@gmail.com>
2023-02-16 15:55:12 +0300

    Separate logo to multiple components

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,55 +1,74 @@
-import React from "react"
-import { View, Text, ViewProps, TextProps } from 'react-native'
+import {
+    View,
+    Text,
+    ViewProps,
+    Platform,
+    Image,
+    StyleSheet,
+} from 'react-native'
 import LogoSvg from '../assets/images/logo.svg'
 
-interface LogoTextProps {
-    textProps?: TextProps
-}
-
-const LogoText: React.FC<LogoTextProps> = ({
-    textProps,
-}) => {
-    return <Text
-        {...textProps}
-        style={{
-            color: '#921C2E',
-            textAlign: 'center',
-            fontSize: 37,
-            fontWeight: '600',
-        }}
-    >
+const LogoText: React.FC = () => {
+    return <Text style={style.logo__text}>
         QUOTEX
     </Text>
 }
 
+interface LogoIconProps {
+    viewStyle?: ViewProps['style']
+}
+
+const LogoIcon: React.FC<LogoIconProps> = ({
+    viewStyle,
+}) => {
+    return <View style={viewStyle}>
+        { Platform.OS === 'web'
+            ? <Image
+                source={require('../assets/images/logo.svg')}
+                style={{ ...style.logo__icon}}
+            />
+            : <LogoSvg />
+        }
+    </View>
+}
+
 interface LogoProps {
     withText?: boolean
-    viewProps?: ViewProps
 }
 
 const Logo: React.FC<LogoProps> = ({
     withText = false,
-    viewProps,
 }) => {
-    return <View
-        {...viewProps}
-        style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            marginBottom: withText ? 16 : undefined,
-        }}
-    >
-        <LogoSvg
-            width={116}
-        />
+    return <View style={style.logo}>
+        <LogoIcon viewStyle={{ marginBottom: withText ? 16 : undefined }} />
         { withText && <LogoText />}
     </View>
 }
 
+const style = StyleSheet.create({
+    logo: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    logo__icon: {
+        width: 116,
+        height: 134,
+    },
+    logo__text: {
+        color: '#921C2E',
+        textAlign: 'center',
+        fontSize: 37,
+        fontWeight: '600',
+        textTransform: 'uppercase',
+    }
+})
+
 export default Logo
 export {
+    LogoSvg,
     LogoText,
-    LogoTextProps,
+    LogoIcon,
+    LogoIconProps,
     LogoProps,
 }
