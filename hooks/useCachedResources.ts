fc6d2fdaa41d6995064cbca2a1daf499a8056100
Timur <timurtokaev.toombez@gmail.com>
2023-02-20 13:45:51 +0300

    Add tasks to useCachedResources

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -1,33 +1,54 @@
-import { FontAwesome } from '@expo/vector-icons';
-import * as Font from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect, useState } from 'react';
+import { FontAwesome } from '@expo/vector-icons'
+import * as Font from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+import { useEffect, useState } from 'react'
+
+type TaskFunction = () => Promise<unknown>
 
 export default function useCachedResources() {
-  const [isLoadingComplete, setLoadingComplete] = useState(false);
+    const [isLoadingComplete, setLoadingComplete] = useState(false)
+    const [progress, setProgress] = useState(0)
 
-  // Load any resources or data that we need prior to rendering the app
-  useEffect(() => {
-    async function loadResourcesAndDataAsync() {
-      try {
-        SplashScreen.preventAutoHideAsync();
-
-        // Load fonts
-        await Font.loadAsync({
-          ...FontAwesome.font,
-          'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
-        });
-      } catch (e) {
-        // We might want to provide this error information to an error reporting service
-        console.warn(e);
-      } finally {
-        setLoadingComplete(true);
-        SplashScreen.hideAsync();
-      }
+    async function loadFontsTask() {
+        return Font.loadAsync({
+            ...FontAwesome.font,
+            'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
+        })
     }
 
-    loadResourcesAndDataAsync();
-  }, []);
+    function emptyTask(ms: number) {
+        return () => new Promise(res => {
+            setTimeout(() => res(true), ms)
+        })
+    }
 
-  return isLoadingComplete;
+    useEffect(() => {
+        console.log(progress);
+    }, [progress])
+
+    async function awaitTasks(...tasks: TaskFunction[]) {
+        const taskProgress = 1 / tasks.length
+
+        try {
+            SplashScreen.preventAutoHideAsync()
+            await Promise.all(tasks.map(async task => {
+                await task()
+                return setProgress(p => p + taskProgress)
+            }))
+        } catch (e) {
+            console.warn(e)
+        } finally {
+            setLoadingComplete(true)
+            SplashScreen.hideAsync()
+        }
+    }
+
+    useEffect(() => {
+        awaitTasks(loadFontsTask, emptyTask(10000), emptyTask(5000))
+    }, [])
+
+    return {
+        isLoadingComplete,
+        progress,
+    }
 }
