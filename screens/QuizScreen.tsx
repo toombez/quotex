354170259fd6799770af12143e183055ae8057b1
Timur <timurtokaev.toombez@gmail.com>
2023-02-22 03:47:41 +0300

    Add QuizScreenStep

--- screens/QuizScreen.tsx
+++ screens/QuizScreen.tsx
@@ -34,10 +34,49 @@ const QuizScreenButton: React.FC<QuizScreenButtonProps> = ({
     </Pressable>
 }
 
-const QuizScreen: React.FC = () => {
-    return <View>
+interface QuizScreenStepAnswer {
+    label: string
+    value: unknown
+}
 
+interface QuizScreenStepProps {
+    question: string
+    answers: QuizScreenStepAnswer[]
+    onAnswer?: (answer: QuizScreenStepAnswer) => void
+}
+
+const QuizScreenStep: React.FC<QuizScreenStepProps> = ({
+    answers,
+    question,
+    onAnswer,
+}) => {
+    const onPressHandler = (answer: QuizScreenStepAnswer) => {
+        if (!onAnswer) return
+
+        onAnswer(answer)
+    }
+
+    return <View>
+        <Text style={QuizScreenStepStyle.question}>
+            { question }
+        </Text>
+        { answers.map((a, i) =>{
+            const isLastAnswer = i === answers.length - 1
+
+            return <QuizScreenButton
+                title={ a.label }
+                pressableStyle={{
+                    marginBottom: isLastAnswer ? 0 : 16,
+                }}
+                onPress={() => onPressHandler(a)}
+                key={i}
+            />})
+        }
     </View>
+}
+
+const QuizScreen: React.FC =  () => {
+    return <View></View>
 }
 
 const buttonStyle = StyleSheet.create({
@@ -57,6 +96,15 @@ const buttonStyle = StyleSheet.create({
     },
     'button__text--pressed': {
         color: '#FFFFFF',
+    }
+})
+
+const QuizScreenStepStyle = StyleSheet.create({
+    question: {
+        fontSize: 28,
+        fontWeight: '800',
+        color: '#1F1F1F',
+        marginBottom: 20,
     }
 })
 
